8b7406a8abcd1ad2d25068914676bed5016c9784
tamal.sen <tamal.sen@hotmail.com>
2021-10-25 22:53:41 +0530

    signly linked list
    all the methods, with comments

---
+++ SinglyLinkedList/Linkedlist.java
@@ -0,0 +1,152 @@
+
+public class Linkedlist {
+
+    Node head;
+
+    // method to insert a node at start when head is empty , if head is not empty,
+    // add node at the end position
+    void insert(int data) {
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = new Node();
+            n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+
+    }
+
+    // method to print all the node in sigly linked list
+    void print() {
+        Node n = new Node();
+        n = head;
+
+        while (n.next != null) {
+            System.out.println(n.data);
+            n = n.next;
+        }
+        System.out.println(n.data);
+    }
+
+    // method to add a node at the first position of a linked list
+    void insertAtFirst(int data) {
+        Node node = new Node();
+
+        node.data = data;
+        node.next = null;
+        node.next = head;
+        head = node;
+
+    }
+
+    // insert a node at the specified index position
+    void insertAtIndex(int index, int data) {
+
+        if (index == 0) {
+            insertAtFirst(data);
+        } else {
+
+            Node node = new Node();
+            node.data = data;
+            node.next = null;
+
+            Node n = head;
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+
+            node.next = n.next;
+            n.next = node;
+
+        }
+
+    }
+
+    // method to delete a node by specifing the index postion of a linked list
+    void deleteAtIndex(int index) {
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node n = head;
+            Node n1 = null;
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            n1 = n.next;
+            n.next = n1.next;
+            n1 = null;
+        }
+
+    }
+
+    // methods to return the lenght of a singly linked list
+    int length() {
+
+        int len = 0;
+        if (head == null) {
+            len = 0;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+                len++;
+                // System.out.println(len);
+            }
+            len = len + 1;
+
+        }
+        return len;
+    }
+
+    // find the middle element of a singly linked list
+    void middleElem() {
+        int end = length();
+        int mid = end / 2;
+        Node n = head;
+        for (int i = 0; i < mid; i++) {
+            n = n.next;
+        }
+        System.out.println(n.data);
+
+    }
+
+    // search an element in singly linkded list
+    boolean search(int elem) {
+        boolean ans = false;
+        Node n = head;
+
+        while (n.next != null) {
+            if (n.data == elem) {
+                ans = true;
+            }
+            n = n.next;
+
+        }
+        if (n.data == elem) {
+            ans = true;
+        }
+        return ans;
+
+    }
+
+    // driver code to execute all the methods
+    public static void main(String[] args) {
+        Linkedlist ll = new Linkedlist();
+        ll.insert(10); // 0
+        ll.insert(20); // 1
+        ll.insert(70); // 2
+        ll.insert(100); // 3
+        ll.insert(75); // 4
+        ll.deleteAtIndex(2);
+        System.out.println(ll.length());
+        ll.middleElem();
+        System.out.println(ll.search(0));
+    }
+
+}
